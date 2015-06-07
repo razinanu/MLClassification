b4fd9bd33639b97f8379ecdec92ad061017b403c
RaziNanu <razi.ghassemi@gmail.com>
2015-06-07 12:30:49 +0200

    PCA AttributeSelect

--- Ramija/src/org/weka/main.java
+++ Ramija/src/org/weka/main.java
@@ -1,50 +1,30 @@
 package org.weka;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 
-import weka.attributeSelection.PrincipalComponents;
-import weka.classifiers.Classifier;
-import weka.classifiers.Evaluation;
-import weka.classifiers.rules.DecisionTable;
-import weka.classifiers.trees.DecisionStump;
-import weka.classifiers.trees.J48;
 import weka.core.Instances;
-import weka.core.converters.ArffLoader.ArffReader;
 import weka.core.converters.ConverterUtils.DataSource;
 
 public class main {
 
-	public static void main(String[] args) throws Exception{
-		
+	public static void main(String[] args) throws Exception {
+
 		System.out.println("Hello Weka!");
-		Instances training = loadFiles("lib/trainSet.arff");
-		Instances test = loadFiles("lib/test.arff");
+		Instances trainSet = loadFiles("lib/trainSet.arff");
+		Instances testSet = loadFiles("lib/test.arff");
+		preProcess attSelect = new preProcess();
+
+		attSelect.AttributeSelect(trainSet, testSet);
 		
-		Classifier[] models = { 
-				new J48(), // a decision tree
-				new DecisionTable(),//decision table majority classifier
-				new DecisionStump() //one-level decision tree
-		};
-		
-	
-		PrincipalComponents pca = new PrincipalComponents();
-		pca.buildEvaluator(training);
-		Instances transformed_train = pca.transformedData(training);
-		
-		System.out.println("done");
+
 	}
 
-	private static Instances loadFiles(String address) throws Exception
-	{
+	private static Instances loadFiles(String address) throws Exception {
 		DataSource source = new DataSource(address);
 		Instances data = source.getDataSet();
 		if (data.classIndex() == -1)
 			data.setClassIndex(data.numAttributes() - 1);
-		
+
 		System.out.println("loaded files. " + address);
 		return data;
 	}
-	
+
 }
---
+++ Ramija/src/org/weka/preProcess.java
@@ -0,0 +1,55 @@
+package org.weka;
+
+import weka.core.*;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Add;
+import weka.filters.unsupervised.attribute.PrincipalComponents;
+
+public class preProcess {
+
+	public void AttributeSelect(Instances trainSet, Instances testSet) {
+
+		Instances newTestSet=null;
+		newTestSet=addClassinTestSet(testSet);
+		PrincipalComponents pca = new PrincipalComponents();
+		try {
+			pca.setInputFormat(trainSet);
+			Instances newTrain = Filter.useFilter(trainSet, pca);
+			Instances newTest = Filter.useFilter(newTestSet, pca);
+			System.out.println("Number of att in Train: "+newTrain.numAttributes());
+			System.out.println("Number of att in Test: "+newTest.numAttributes());
+			for (int i = 0; i < newTrain.numAttributes(); i++) {
+				System.out.println("Train: "+newTrain.attribute(i));
+				
+			}
+			for (int i = 0; i < newTest.numAttributes(); i++) {
+				System.out.println("Test: "+newTest.attribute(i));
+				
+			}
+
+		} catch (Exception exc) {
+			System.out.println(exc.getMessage());
+		}
+	}
+
+	public Instances addClassinTestSet(Instances testSet) {
+
+		Add filter = new Add();
+		Instances newTestSet=null;
+
+		filter.setAttributeIndex("last");
+		filter.setNominalLabels("Class_1,Class_2,Class_3,Class_4,Class_5,Class_6,Class_7,Class_8,Class_9");
+		filter.setAttributeName("class");
+		try {
+			filter.setInputFormat(testSet);
+			newTestSet = Filter.useFilter(testSet, filter);
+			System.out.println("Test set num Attribute: "
+					+ newTestSet.numAttributes());
+		} catch (Exception exc) {
+			System.out.println(exc.getMessage());
+		}
+		return newTestSet;
+
+	}
+
+}
