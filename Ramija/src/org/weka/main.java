9a55585cb68f038937fd7923ce73d87a228aa74a
jsabsch <jm-fuchs@web.de>
2015-06-12 14:01:05 +0200

    start semi-supervised (SSS)

--- Ramija/src/org/weka/main.java
+++ Ramija/src/org/weka/main.java
@@ -4,6 +4,7 @@ import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.unsupervised.attribute.Add;
 
+
 public class main {
 	
 	
@@ -18,7 +19,8 @@ public class main {
 
 		attSelect.AttributeSelect(trainSet, testSet);
 
-	
+		SemiSupervised s = new SemiSupervised();
+		s.classify(trainSet, testSet);
 	}
 
 	private static Instances loadFiles(String address) throws Exception {
