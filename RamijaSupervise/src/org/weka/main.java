784fa6849009e8bbb815ca86abf2d4c318e18cc8
RaziNanu <razi.ghassemi@gmail.com>
2015-06-12 15:32:40 +0200

    change the name of project

---
+++ RamijaSupervise/src/org/weka/main.java
@@ -0,0 +1,37 @@
+package org.weka;
+
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+public class main {
+	
+	
+
+	public static void main(String[] args) throws Exception {
+
+		
+		 Instances trainSet = loadFiles("lib/trainSet.arff");
+	     Instances testSet = loadFiles("lib/test.arff");
+	    
+
+
+		PreProcess attSelect = new PreProcess();
+
+		attSelect.AttributeSelect(trainSet, testSet);
+		Classifiers c= new Classifiers();
+		c.classifier(attSelect.newTrain, attSelect.newTest);
+
+	
+	}
+
+	private static Instances loadFiles(String address) throws Exception {
+		DataSource source = new DataSource(address);
+		Instances data = source.getDataSet();
+		if (data.classIndex() == -1)
+			data.setClassIndex(data.numAttributes() - 1);
+		
+		System.out.println("loaded files: " + address);
+		return data;
+	}
+
+}
