1bb3b0b3fcf7f2b921dea6c6c9a9fd8a85694638
jsabsch <jm-fuchs@web.de>
2015-06-14 13:05:03 +0200

    corrected the merge

--- Ramija/src/org/weka/SemiSupervised.java
+++ Ramija/src/org/weka/SemiSupervised.java
@@ -6,37 +6,48 @@ import weka.core.Instances;
 
 public class SemiSupervised {
 
+	private boolean[] used;
+	
 	public SemiSupervised()
 	{
-		
 	}
 	
 	public void classify(Instances labeled, Instances unlabeled) throws Exception
 	{
-		System.out.println("start semi-supervised classification...");
+		System.out.println("start semi-supervised classification.");
 		
+		used = new boolean[unlabeled.numInstances()];
 		int iter = 0;
+		
 		while(unlabeled.numInstances() > 0)
-		{
+		{		
+			System.out.println("iteration " + iter++ + "(count: " + labeled.numInstances() + " " + unlabeled.numInstances() + ")");
+
 			IBk classifier = new IBk(5);
-			
-			System.out.println("iteration " + iter++ + " (count: " + labeled.numInstances() + " " + unlabeled.numInstances() + ")");
-			
 			classifier.buildClassifier(labeled);
 
-			int random_instance = (int)(Math.random() * (unlabeled.numInstances()-1));
+			int random_instance = findRandomInstance();
+			
 			double cl = classifier.classifyInstance(unlabeled.instance(random_instance));
 			unlabeled.instance(random_instance).setClassValue(cl);
 			labeled.add((Instance)unlabeled.instance(random_instance).copy());
-			unlabeled.delete(random_instance);		
-		}
-		
-		for(int i=0; i<labeled.numInstances(); i++)
-		{
-			System.out.println(labeled.instance(i).classValue());
 		}
 	}
-	
+		
+	private int findRandomInstance()
+	{
+		int random_instance = (int)(Math.random() * (used.length));
+		
+		while (used[random_instance])
+		{
+			random_instance++;
+			if(random_instance > used.length)	random_instance = 0;
+		}
+		
+		used[random_instance] = true;
+		
+		return random_instance;
+	}
 	
 	
 }
